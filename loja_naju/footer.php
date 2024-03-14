c50d7d7571e88e70bed8cb7f203fcf26e321d439
anaju101084 <163453638+anaju101084@users.noreply.github.com>
2024-03-14 15:23:04 -0300

    Add files via upload
    commit realizado com alterações aplicadas na aula do dia 14 de março

---
+++ loja_naju/footer.php
@@ -0,0 +1,9 @@
+</div>
+
+    <!-- container que abrigará o rodapé da página -->
+    <div class="container-fluid text-center bg-dark text-light fixed-bottom ">
+        <p>Sistema Web desenvolvido por Naju</p>
+    </div>
+
+</body>
+</html>
---
+++ loja_naju/index.php
@@ -0,0 +1,25 @@
+<?php include("header php")?>
+
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        <h1> Aqui ficará o conteúdo da página</h1>
+        
+    <?php include("fooder.php") ?>
